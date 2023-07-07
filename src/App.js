adb7c200fc1a81d0c6663b0a4e16a3a41b8c641c
Renato Albuquerque <renatomalb83@gmail.com>
2023-07-07 00:37:54 -0300

    initial setup project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React !!!</h1>
-      <p>My second App.</p>
+      <p>Costs</p>
     </div>
   );
 }
